2864e1a13337ac19acbec7ac2fb154ed46d79d9a
Richard Tew <richard.m.tew@gmail.com>
2014-10-27 17:53:30 +1300

    Prerelease 0.6.9Y13 version numbering changes.

--- inc/Defines.h
+++ inc/Defines.h
@@ -10,7 +10,7 @@
 
 #define SIGNATURE 0x1234ABCD
 #define SIGNATURE_TWO 0xF1F2F3F4
-#define VERSION_STRING "0.6.9Y12"
+#define VERSION_STRING "0.6.9Y13"
 
 #define SAVE_SUFFIX ".sav"
 #define BACKUP_SUFFIX ".backup"
