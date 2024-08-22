fd9cff118da58fd607f8adc357d6851dabcd16fa
Richard Tew <richard.m.tew@gmail.com>
2024-08-22 21:19:13 +1200

    Bump the version number so VS2015 32bit saves from 0.6.9Y19 cannot be loaded any more.

--- inc/Defines.h
+++ inc/Defines.h
@@ -14,7 +14,7 @@
 
 #define SIGNATURE 0x1234ABCD
 #define SIGNATURE_TWO 0xF1F2F3F4
-#define VERSION_STRING "0.6.9Y19"
+#define VERSION_STRING "0.6.9Y20a1"
 
 #define SAVE_SUFFIX ".sav"
 #define BACKUP_SUFFIX ".backup"
